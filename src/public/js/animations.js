7716f874b3c0d7031c2f83b9da1ac0cdff0c6290
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-06-12 15:48:22 -0500

    update views: Add preventDefault on forms(sign-in, sign-up, search-user and send message), Add fake links to switch sign-up and sign-in,

--- src/public/js/animations.js
+++ src/public/js/animations.js
@@ -5,3 +5,23 @@ navOptionsBtn.addEventListener('click', e => {
     e.preventDefault;
     navOptionsContent.classList.toggle('active')
 });
+
+// sections
+const login = document.querySelector('.login');
+const registration = document.querySelector('.registration');
+const globalChat = document.querySelector('.global-chat');
+
+// small links
+const smSignUp = document.getElementById('smSignUp');
+const smSignIn = document.getElementById('smSignIn');
+
+smSignIn.addEventListener('click', e => {
+    registration.style.display = 'none';
+    login.style.display = 'block';
+});
+
+
+smSignUp.addEventListener('click', e => {
+    login.style.display = 'none';
+    registration.style.display = 'block';
+});
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,40 @@
 io();
+
+
+/** Sign up */
+const formSignUp = document.getElementById('registration');
+formSignUp.addEventListener('submit', e => {
+    e.preventDefault();
+})
+
+
+/** Sign in */
+const formSignIn = document.getElementById('login');
+formSignIn.addEventListener('submit', e => {
+    e.preventDefault();
+});
+
+/** Logout */
+const logoutBtn = document.getElementById('logout');
+
+/** Change image */
+const changeImage = document.getElementById('changeImage');
+
+/** Search user */
+const searchUser = document.getElementById('searchUser');
+const searchUserInput = document.getElementById('searchUserInput');
+searchUserInput.addEventListener('keyup', e => {           
+    
+})
+searchUser.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log(searchUserInput.value)
+});
+
+/** Send message */
+const sendMessage = document.getElementById('sendMessage');
+const sendMessageInput = document.getElementById('sendMessageInput');
+sendMessage.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log(sendMessageInput.value)
+});
